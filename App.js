7fac43e8c3815bb0dc873fa8239e27656ed7ffc8
lexexpert <ivanreshetar@gmail.com>
2019-06-04 15:02:23 -0500

    Final updates

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from "redux";
 import reducers from './src/reducers';
 import {Header} from './src/components/common/Header';
+import LibraryList from './src/components/LibraryList';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
+        <View style={{flex: 1}}>
           <Header headerText={'Tech Stack'} />
+          <LibraryList/>
         </View>
       </Provider>
     );
